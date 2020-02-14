2db29e8428d48fcf9cdfb4b1fe89738bb5f40fd1
zs5460 <zs5460@gmail.com>
2020-02-14 17:33:39 +0800

    add fonts && vendor

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
 #build
 FROM golang AS build
 
-RUN go env -w GOPROXY=https://goproxy.cn,direct 
-
 WORKDIR /app
 
 ADD . .
 
-RUN CGO_ENABLED=0 GOOS=linux go build . 
+RUN CGO_ENABLED=0 GOOS=linux go build -mod=vendor . 
 
 #production
 FROM leanix/phantomjs2 AS prod
 WORKDIR /app
-COPY --from=build /app/p.js . 
+COPY --from=build /app/fonts/* /usr/share/fonts/
+COPY --from=build /app/snapshot.js . 
 COPY --from=build /app/website-snapshot .
 
 CMD ["./website-snapshot"]
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"time"
 
 	"github.com/qiniu/api.v7/auth/qbox"
 	"github.com/qiniu/api.v7/storage"
@@ -18,9 +20,22 @@ var (
 	bucket    = os.Getenv("QINIU_BUCKET")
 	url       = os.Getenv("URL")
 	prefix    = "snapshot/"
+	author    = "zs5460"
+	version   = "1.0.9"
 )
 
 func main() {
+	banner := `
+                                     __            __ 
+   _____ ____   ____   ____   _____ / /_   ____   / /_
+  / ___// __ \ / __ \ / __ \ / ___// __ \ / __ \ / __/
+ (__  )/ / / // /_/ // /_/ /(__  )/ / / // /_/ // /_  
+/____//_/ /_/ \__,_// .___//____//_/ /_/ \____/ \__/  
+                   /_/                                
+`
+	fmt.Println(banner)
+	fmt.Printf("author:%s version:%s\n", author, version)
+
 	if accessKey == "" || secretKey == "" || bucket == "" || url == "" {
 		log.Println("Please set environment variables first.")
 		os.Exit(1)
@@ -35,9 +50,8 @@ func main() {
 }
 
 func snap(url string) {
-	log.Println("will snap ", url)
-	c := exec.Command("/bin/sh", "-c", "phantomjs", "/app/p.js", url)
-	c.Dir = "/app"
+	c := exec.Command("/bin/bash", "-c", "phantomjs snapshot.js "+url)
+	//c.Dir = filepath.Dir(os.Args[0])
 	c.Stdout = os.Stdout
 	err := c.Run()
 	if err != nil {
@@ -55,21 +69,20 @@ func upload(localFile, key string) {
 	ret := storage.PutRet{}
 	err := formUploader.PutFile(context.Background(), &ret, upToken, key, localFile, nil)
 	if err != nil {
-		fmt.Println(key, err)
+		log.Println(key, err)
 		return
 	}
-	fmt.Println(ret.Key, ret.Hash)
+	log.Println(ret.Key)
 }
 
 func run() {
-	tmpfile := "/app/data/tmp.png"
-	log.Println("tmpfile:", tmpfile)
-	//key := prefix + time.Now().Format("200601021504") + ".png"
+	tmpfile := filepath.Join(filepath.Dir(os.Args[0]), "tmp.png")
+	key := prefix + time.Now().Format("20060102150405") + ".png"
 	log.Println("start snapshot...")
 	snap(url)
-	// log.Println("start upload...")
-	// upload(tmpfile, key)
-	// log.Println("start clean...")
-	// os.Remove(tmpfile)
+	log.Println("start upload...")
+	upload(tmpfile, key)
+	log.Println("start clean...")
+	os.Remove(tmpfile)
 	log.Println("completed!")
 }
